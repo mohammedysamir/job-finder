a9fe7fb1bfe091f7d04a5468eb15b7f11e0091e5
mohammed-yasser <mohammed.yasser@vodafone.com>
2025-07-11 21:12:55 +0200

    Create template for PostController and AdminController

--- src/main/java/com/jobfinder/finder/controller/AdminController.java
+++ src/main/java/com/jobfinder/finder/controller/AdminController.java
@@ -1,9 +1,42 @@
 package com.jobfinder.finder.controller;
 
+import com.jobfinder.finder.dto.AdminCreationDto;
+import com.jobfinder.finder.dto.AdminResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/admin")
 @Slf4j
 public class AdminController {
+  @GetMapping("/{username}")
+  public ResponseEntity<AdminResponseDto> getAdmin(@PathVariable String username) {
+    log.info("Fetching admin details for username: {}", username);
+    return new ResponseEntity<>(null, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{username}")
+  public ResponseEntity<AdminResponseDto> deleteAdmin(@PathVariable String username) {
+    log.info("Deleting an admin with username: {}", username);
+    return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+  }
+
+  @PatchMapping("/{username}")
+  public ResponseEntity<AdminResponseDto> updateAdmin(@PathVariable String username) {
+    log.info("Patching an admin with username: {}", username);
+    return new ResponseEntity<>(null, HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<AdminResponseDto> createAdmin(@RequestBody AdminCreationDto dto) {
+    log.info("Creating an admin: {}", dto.toString());
+    return new ResponseEntity<>(null, HttpStatus.CREATED);
+  }
 }
--- src/main/java/com/jobfinder/finder/controller/PostController.java
+++ src/main/java/com/jobfinder/finder/controller/PostController.java
@@ -1,9 +1,60 @@
 package com.jobfinder.finder.controller;
 
+import com.jobfinder.finder.constant.PostStatus;
+import com.jobfinder.finder.dto.PostDto;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/post")
 @Slf4j
 public class PostController {
+  @GetMapping
+  public ResponseEntity<List<PostDto>> getPosts(@RequestParam String filter) {
+    if (filter != null && !filter.isEmpty()) {
+      log.info("Fetching posts with filter: {}", filter);
+      // Here you would typically call a service to fetch the posts based on the filter
+      //todo: implement filtering logic
+      return new ResponseEntity<>(List.of(), HttpStatus.OK);
+    }
+    log.info("Fetching all posts");
+    return new ResponseEntity<>(List.of(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<PostDto> createPost(PostDto dto) {
+    log.info("Creating a new post with filter: {}", dto);
+    // Here you would typically call a service to create the post
+    return new ResponseEntity<>(dto, HttpStatus.CREATED);
+  }
+
+  @PatchMapping("/{postId}")
+  public ResponseEntity<PostDto> updatePost(@PathVariable String postId, PostDto dto) {
+    log.info("Updating post with ID: {} with data: {}", postId, dto);
+    // Here you would typically call a service to update the post
+    return new ResponseEntity<>(dto, HttpStatus.OK);
+  }
+
+  @PatchMapping("/{postId}/status")
+  public ResponseEntity<Void> patchPostStatus(@PathVariable String postId, @RequestParam PostStatus status) {
+    log.info("Update post status with ID: {} to {}", postId, status);
+    // Here you would typically call a service to close the post
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @DeleteMapping("/{postId}")
+  public ResponseEntity<Void> deletePost(@PathVariable String postId) {
+    log.info("Deleting post with ID: {}", postId);
+    // Here you would typically call a service to delete the post
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
 }
