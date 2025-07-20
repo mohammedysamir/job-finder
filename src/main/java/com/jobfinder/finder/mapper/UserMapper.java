d7a45eea1e32e967c41a58c9f81d589395e02d4e
mohammed-yasser <mohammed.yasser@vodafone.com>
2025-07-20 10:42:46 +0200

    Define mapper and repository for User and update UserService

--- src/main/java/com/jobfinder/finder/mapper/UserMapper.java
+++ src/main/java/com/jobfinder/finder/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import com.jobfinder.finder.entity.UserEntity;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface UserDtoMapper {
+public interface UserMapper {
   UserResponseDto toDto(UserEntity userEntity);
 
   UserEntity toEntity(UserRegistrationDto userDto);
--- src/main/java/com/jobfinder/finder/service/UserService.java
+++ src/main/java/com/jobfinder/finder/service/UserService.java
@@ -3,33 +3,69 @@ package com.jobfinder.finder.service;
 import com.jobfinder.finder.dto.UserPatchDto;
 import com.jobfinder.finder.dto.UserRegistrationDto;
 import com.jobfinder.finder.dto.UserResponseDto;
+import com.jobfinder.finder.entity.UserEntity;
+import com.jobfinder.finder.mapper.UserMapper;
+import com.jobfinder.finder.repository.UserRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class UserService {
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
+
+  public UserService(UserRepository userRepository, UserMapper userMapper) {
+    this.userRepository = userRepository;
+    this.userMapper = userMapper;
+  }
 
   public UserResponseDto registerUser(UserRegistrationDto dto) {
     log.info("Registering a new user: {}", dto.toString());
-    //todo: map to entity and save to database
-    return null; // Return the registered user details
+    UserEntity entity = userMapper.toEntity(dto);
+    userRepository.save(entity);
+    return userMapper.toDto(entity);
   }
 
   public UserResponseDto getUserProfile(String username) {
     log.info("Fetching user profile for username: {}", username);
-    //todo: fetch from database
-    return null;
+    Optional<UserEntity> optionalUserEntity = userRepository.findByUsername(username);
+    if (optionalUserEntity.isPresent()) {
+      UserEntity userEntity = optionalUserEntity.get();
+      return userMapper.toDto(userEntity);
+    } else {
+      log.warn("User with username {} not found", username);
+      throw new UsernameNotFoundException("User not found with username: " + username);
+    }
   }
 
   public UserResponseDto updateUserProfile(String username, UserPatchDto dto) {
     log.info("Updating user profile for username: {} with data: {}", username, dto.toString());
-    //todo: map to entity and update in database
-    return null;
+    Optional<UserEntity> optionalUserEntity = userRepository.findByUsername(username);
+    if (optionalUserEntity.isPresent()) {
+      UserEntity userEntity = optionalUserEntity.get();
+      UserEntity updatedEntity = userMapper.toEntity(dto);
+      updatedEntity.setId(userEntity.getId());
+
+      userRepository.save(updatedEntity);
+      return userMapper.toDto(updatedEntity);
+    } else {
+      log.warn("User with username {} not found", username);
+      throw new UsernameNotFoundException("User not found with username: " + username);
+    }
   }
 
   public void deleteUser(String username) {
     log.info("Deleting a user for username: {}", username);
-    //todo: delete from database
+    Optional<UserEntity> optionalUserEntity = userRepository.findByUsername(username);
+    if (optionalUserEntity.isPresent()) {
+      userRepository.deleteById(optionalUserEntity.get().getId());
+    } else {
+      log.warn("User with username {} not found", username);
+      throw new UsernameNotFoundException("User not found with username: " + username);
+    }
   }
 }
