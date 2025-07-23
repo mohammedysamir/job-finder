a17870c43b750101ac6a74696709669089b8dd2b
mohammed-yasser <mohammed.yasser@vodafone.com>
2025-07-23 19:53:10 +0200

    fix all tests related to PostController

--- src/test/java/com/jobfinder/finder/integrationTest/controller/PostControllerIntegrationTest.java
+++ src/test/java/com/jobfinder/finder/integrationTest/controller/PostControllerIntegrationTest.java
@@ -56,12 +56,6 @@ public class PostControllerIntegrationTest {
   @MockBean
   private PostService postService;
 
-  @MockBean
-  private PostRepository postRepository;
-
-  private final int page = 0;
-  private final int size = 10;
-
   @BeforeEach
   void setUp() {
     mockMvc = MockMvcBuilders
@@ -71,8 +65,8 @@ public class PostControllerIntegrationTest {
   }
 
   @Test
+  @WithUserDetails("applicant")
   void getPosts_happy() throws Exception {
-    PageRequest pageRequest = PageRequest.of(page, size);
 
     List<PostDto> response = List.of(
         new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
@@ -80,21 +74,12 @@ public class PostControllerIntegrationTest {
         new PostDto("Title2", "Description2", PostStatus.CLOSED, "Location2", "Company2", EmploymentType.PART_TIME, 1, 3, List.of("Python", "Django"),
             "recruiter2")
     );
-    Mockito.when(postService.getPosts(new PostFilterRequestDto(), page, size)).thenReturn(
+    Mockito.when(postService.getPosts(Mockito.any(PostFilterRequestDto.class), Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(
         response
     );
-    Page<PostEntity> pageResponse = new PageImpl<>(
-        response.stream().map(postDto -> new PostEntity(null, postDto.getTitle(), postDto.getDescription(), postDto.getStatus(),
-            postDto.getLocation(), postDto.getCompanyName(), postDto.getEmploymentType(), postDto.getMinimumExperience(),
-            postDto.getMaximumExperience(), postDto.getSkillsRequired(), postDto.getRecruiterUsername())).toList(),
-        pageRequest, response.size()
-    );
-
-    Mockito.when(postRepository.findAll(pageRequest)).thenReturn(pageResponse);
     // When
     mockMvc.perform(
             MockMvcRequestBuilders.get("/post")
-                .with(user("applicant").password("applicant").roles("APPLICANT"))
                 .param("page", "0")
                 .param("size", "10")
         ).andExpect(status().isOk())
@@ -102,11 +87,12 @@ public class PostControllerIntegrationTest {
   }
 
   @Test
+  @WithUserDetails("applicant")
   void getPosts_withFilters_happy() throws Exception {
     // Given
     PostFilterRequestDto filter = new PostFilterRequestDto(null, "Title1", "Location1", null, 0, 0, null, null);
 
-    Mockito.when(postService.getPosts(filter, 0, 10)).thenReturn(
+    Mockito.when(postService.getPosts(Mockito.any(PostFilterRequestDto.class), Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(
         List.of(
             new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
                 "recruiter1")
@@ -115,13 +101,34 @@ public class PostControllerIntegrationTest {
     // When
     mockMvc.perform(
             MockMvcRequestBuilders.get("/post")
-                .with(user("applicant").password("applicant").roles("APPLICANT"))
                 .param("page", "0")
                 .param("size", "10")
                 .param("title", "Title1")
                 .param("location", "Location1")
         ).andExpect(status().isOk())
-        .andExpect(jsonPath("$.length()").value(1));
+        .andExpect(jsonPath("$.length()").value(1))
+        .andExpect(jsonPath("$[0].title").value(filter.getTitle()))
+        .andExpect(jsonPath("$[0].location").value(filter.getLocation()));
+  }
+
+  @Test
+  @WithUserDetails("admin")
+  void getPosts_unauthorized() throws Exception {
+    // Given
+    Mockito.when(postService.getPosts(Mockito.any(PostFilterRequestDto.class), Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(
+        List.of(
+            new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
+                "recruiter1")
+        )
+    );
+    // When
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/post")
+            .param("page", "0")
+            .param("size", "10")
+            .param("title", "Title1")
+            .param("location", "Location1")
+    ).andExpect(status().isForbidden());
   }
 
   @Test
