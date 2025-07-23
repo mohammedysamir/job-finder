c1a16bb010f9e44dce689fc98bbff99f3eeb0b54
mohammed-yasser <mohammed.yasser@vodafone.com>
2025-07-23 19:43:50 +0200

    fixes PostController tests except filter

--- src/test/java/com/jobfinder/finder/integrationTest/configuration/MockUserDetailsManagerConfig.java
+++ src/test/java/com/jobfinder/finder/integrationTest/configuration/MockUserDetailsManagerConfig.java
@@ -1,16 +1,47 @@
 package com.jobfinder.finder.integrationTest.configuration;
 
+import com.jobfinder.finder.constant.Roles;
 import java.util.List;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
 public class MockUserDetailsManagerConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .httpBasic(Customizer.withDefaults())
+        .csrf(customizer -> customizer.disable()) // will use stateless authentication, so CSRF protection is not needed
+        .authorizeHttpRequests(
+            c ->
+                // Define access rules for different endpoints
+                //-- Public endpoints
+                c.requestMatchers("/login", "/register").permitAll() // Allow public access to login and register
+                    //-- Admin related endpoints
+                    .requestMatchers("/actuator/**").hasAnyRole(Roles.SUPER_ADMIN.name(), Roles.ADMIN.name()) // Allow access to actuator endpoints to admins
+                    .requestMatchers("/admin/**").hasRole(Roles.SUPER_ADMIN.name())
+                    //-- User related endpoints
+                    .requestMatchers("/user/**").hasAnyRole(Roles.APPLICANT.name())
+                    //-- Post related endpoints
+                    .requestMatchers(HttpMethod.DELETE, "/post/**")
+                    .hasAnyRole(Roles.RECRUITER.name()
+                        , Roles.ADMIN.name()
+                        , Roles.SUPER_ADMIN.name()) // admins and recruiters can delete posts
+                    .requestMatchers(HttpMethod.GET, "/post/**").hasAnyRole(Roles.APPLICANT.name())
+                    .requestMatchers(HttpMethod.POST, "/post/**").hasAnyRole(Roles.RECRUITER.name())
+                    .requestMatchers(HttpMethod.PATCH, "/post/**").hasAnyRole(Roles.RECRUITER.name())
+                    .anyRequest().authenticated() // Require authentication for all other requests
+        ).build();
+  }
 
   @Bean
   public UserDetailsManager userDetailsManager() {
--- src/test/java/com/jobfinder/finder/integrationTest/controller/PostControllerIntegrationTest.java
+++ src/test/java/com/jobfinder/finder/integrationTest/controller/PostControllerIntegrationTest.java
@@ -1,38 +1,42 @@
 package com.jobfinder.finder.integrationTest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobfinder.finder.constant.EmploymentType;
 import com.jobfinder.finder.constant.PostStatus;
 import com.jobfinder.finder.controller.PostController;
 import com.jobfinder.finder.dto.PostDto;
 import com.jobfinder.finder.dto.PostFilterRequestDto;
+import com.jobfinder.finder.entity.PostEntity;
 import com.jobfinder.finder.integrationTest.configuration.MockUserDetailsManagerConfig;
-import com.jobfinder.finder.mapper.PostMapper;
+import com.jobfinder.finder.repository.PostRepository;
 import com.jobfinder.finder.service.PostService;
-import java.net.URI;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestClientException;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(MockUserDetailsManagerConfig.class)
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@WebMvcTest(PostController.class)
 @EnableAutoConfiguration(exclude = {
     org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
     org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
@@ -40,124 +44,148 @@ import org.springframework.web.client.RestClientException;
 })
 @ActiveProfiles("test")
 public class PostControllerIntegrationTest {
-  private TestRestTemplate testRestTemplate;
+  @Autowired
+  WebApplicationContext context;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
   @MockBean
   private PostService postService;
+
   @MockBean
-  private PostMapper postMapper;
-  @InjectMocks
-  private PostController postController;
-  @LocalServerPort
-  private int port;
+  private PostRepository postRepository;
 
-  @Test
-  void getPosts_noFilters_happy() {
-    // Given
-    testRestTemplate = new TestRestTemplate("applicant", "applicant");
-    String url = "http://localhost:" + port + "/post";
-    Mockito.when(postService.getPosts(null,0,10)).thenReturn(
-        List.of(
-            new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"), "recruiter1"),
-            new PostDto("Title2", "Description2", PostStatus.CLOSED, "Location2", "Company2", EmploymentType.PART_TIME, 1, 3, List.of("Python", "Django"), "recruiter2")
-        )
-    );
-    // When
-    ResponseEntity<List<PostDto>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+  private final int page = 0;
+  private final int size = 10;
 
-    // Then
-    assert response.getStatusCode().is2xxSuccessful();
-    assert response.getBody() != null;
-    assert response.getBody().size() == 2;
+  @BeforeEach
+  void setUp() {
+    mockMvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(springSecurity())
+        .build();
   }
 
   @Test
-  void getPosts_withFilters_happy() {
+  void getPosts_happy() throws Exception {
+    PageRequest pageRequest = PageRequest.of(page, size);
+
+    List<PostDto> response = List.of(
+        new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
+            "recruiter1"),
+        new PostDto("Title2", "Description2", PostStatus.CLOSED, "Location2", "Company2", EmploymentType.PART_TIME, 1, 3, List.of("Python", "Django"),
+            "recruiter2")
+    );
+    Mockito.when(postService.getPosts(new PostFilterRequestDto(), page, size)).thenReturn(
+        response
+    );
+    Page<PostEntity> pageResponse = new PageImpl<>(
+        response.stream().map(postDto -> new PostEntity(null, postDto.getTitle(), postDto.getDescription(), postDto.getStatus(),
+            postDto.getLocation(), postDto.getCompanyName(), postDto.getEmploymentType(), postDto.getMinimumExperience(),
+            postDto.getMaximumExperience(), postDto.getSkillsRequired(), postDto.getRecruiterUsername())).toList(),
+        pageRequest, response.size()
+    );
+
+    Mockito.when(postRepository.findAll(pageRequest)).thenReturn(pageResponse);
+    // When
+    mockMvc.perform(
+            MockMvcRequestBuilders.get("/post")
+                .with(user("applicant").password("applicant").roles("APPLICANT"))
+                .param("page", "0")
+                .param("size", "10")
+        ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.length()").value(2));
+  }
+
+  @Test
+  void getPosts_withFilters_happy() throws Exception {
     // Given
-    testRestTemplate = new TestRestTemplate("applicant", "applicant");
     PostFilterRequestDto filter = new PostFilterRequestDto(null, "Title1", "Location1", null, 0, 0, null, null);
-    String url = "http://localhost:" + port + "/post?" + filter.toString();
 
-    Mockito.when(postService.getPosts(filter,0,10)).thenReturn(
+    Mockito.when(postService.getPosts(filter, 0, 10)).thenReturn(
         List.of(
-            new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"), "recruiter1")
+            new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
+                "recruiter1")
         )
     );
     // When
-    ResponseEntity<List<PostDto>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
-
-    // Then
-    assert response.getStatusCode().is2xxSuccessful();
-    assert response.getBody() != null;
-    assert response.getBody().size() == 1;
+    mockMvc.perform(
+            MockMvcRequestBuilders.get("/post")
+                .with(user("applicant").password("applicant").roles("APPLICANT"))
+                .param("page", "0")
+                .param("size", "10")
+                .param("title", "Title1")
+                .param("location", "Location1")
+        ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.length()").value(1));
   }
 
   @Test
-  void deletePost_happy() {
+  @WithUserDetails("recruiter")
+  void deletePost_happy() throws Exception {
     // Given
-    testRestTemplate = new TestRestTemplate("recruiter", "recruiter");
     String postId = "12345";
-    String url = "http://localhost:" + port + "/post/" + postId;
 
     Mockito.doNothing().when(postService).deletePost(postId);
     // When
-    ResponseEntity<Void> response = testRestTemplate.exchange(url, HttpMethod.DELETE, null, Void.class);
-    assert response.getStatusCode().is2xxSuccessful();
-    assert response.getStatusCodeValue() == 204;
+    mockMvc.perform(MockMvcRequestBuilders.delete("/post/{postId}", postId))
+        .andExpect(status().isNoContent());
   }
 
   @Test
-  void deletePost_unauthorized_403() {
+  @WithUserDetails("applicant")
+  void deletePost_unauthorized_403() throws Exception {
     // Given
-    testRestTemplate = new TestRestTemplate("applicant", "applicant");
     String postId = "12345";
-    String url = "http://localhost:" + port + "/post/" + postId;
 
     Mockito.doNothing().when(postService).deletePost(postId);
     // When
-    ResponseEntity<Void> response = testRestTemplate.exchange(url, HttpMethod.DELETE, null, Void.class);
-    assert response.getStatusCode().is4xxClientError();
-    assert response.getStatusCodeValue() == 403;
+    mockMvc.perform(MockMvcRequestBuilders.delete("/post/{postId}", postId))
+        .andExpect(status().isForbidden());
   }
 
   @Test
-  void createPost_happy() {
+  @WithUserDetails("recruiter")
+  void createPost_happy() throws Exception {
     // Given
-    testRestTemplate = new TestRestTemplate("recruiter", "recruiter");
-    String url = "http://localhost:" + port + "/post";
-    PostDto postDto = new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
+    PostDto postDto = new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5,
+        List.of("Java", "Spring"),
         "recruiter1");
 
     Mockito.when(postService.createPost(Mockito.any(PostDto.class))).thenReturn(postDto);
     // When
-    ResponseEntity<PostDto> response = testRestTemplate.postForEntity(URI.create(url), postDto, PostDto.class);
-
-    // Then
-    assert response.getStatusCode().is2xxSuccessful();
-    assert response.getBody() != null;
-    assert response.getBody().getTitle().equals("Title1");
+    mockMvc.perform(
+            MockMvcRequestBuilders.post("/post")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(postDto))
+        ).andExpect(status().isCreated())
+        .andExpect(jsonPath("$.title").value("Title1"))
+        .andExpect(jsonPath("$.description").value("Description1"));
   }
 
   @Test
-  void createPost_unauthorized_403() {
-    // Given
-    testRestTemplate = new TestRestTemplate("admin", "admin");
-    String url = "http://localhost:" + port + "/post";
-    PostDto postDto = new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5, List.of("Java", "Spring"),
+  void createPost_unauthorized_403() throws Exception {
+    PostDto postDto = new PostDto("Title1", "Description1", PostStatus.ACTIVE, "Location1", "Company1", EmploymentType.FULL_TIME, 2, 5,
+        List.of("Java", "Spring"),
         "recruiter1");
-
-    Mockito.when(postService.createPost(postDto)).thenReturn(postDto);
-    // When
-
-    Assertions.assertThrows(RestClientException.class, () -> testRestTemplate.postForEntity(URI.create(url), postDto, PostDto.class));
+    // Given
+    mockMvc.perform(
+        MockMvcRequestBuilders.post("/post")
+            .with(user("admin").password("admin").roles("admin"))
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(postDto))
+    ).andExpect(status().isForbidden());
   }
 
   @Test
-  void updatePost_happy() {
+  void updatePost_happy() throws Exception {
     // Given
-    testRestTemplate = new TestRestTemplate("recruiter", "recruiter");
     String postId = "12345";
 
-    String url = "http://localhost:" + port + "/post/" + postId;
     PostDto oldPost = new PostDto("old Title", "old Description", PostStatus.ACTIVE, "old Location", "old Company", EmploymentType.FULL_TIME, 3,
         6, List.of("Java", "Spring Boot"), "recruiter1");
 
@@ -167,10 +195,13 @@ public class PostControllerIntegrationTest {
     Mockito.when(postService.updatePost(Mockito.any(String.class), Mockito.any(PostDto.class))).thenReturn(postDto);
 
     // When
-    PostDto response = testRestTemplate.patchForObject(url, oldPost, PostDto.class);
-
-    // Then
-    assert response != null;
-    assert response.getTitle().equals("Updated Title");
+    mockMvc.perform(
+            MockMvcRequestBuilders.patch("/post/{postId}", postId)
+                .with(user("recruiter").password("recruiter").roles("RECRUITER"))
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(postDto))
+        ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.title").value("Updated Title"))
+        .andExpect(jsonPath("$.description").value("Updated Description"));
   }
 }
